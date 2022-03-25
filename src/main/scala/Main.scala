1ff410a68ab81e2eef2d5f05131fb5a414c0f7b9
Florian Falkenberg <fl851fal@htwg-konstanz.de>
2022-03-25 12:51:20 +0100

    Init abstraktes Spielfeld

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,16 @@
+@main def hello: Unit =
+  println(eol + "Welcome to Oware!" + eol)
+  println(mesh(3, cellNum = len_board))
+
+// Game description: http://joansala.com/auale/rules/en/
+val len_board:Int = 6
+
+val eol = sys.props("line.separator")
+def bar(cellWidth: Int = 3, cellNum: Int = 3) =
+  ("+" + "-" * cellWidth) * cellNum + "+" + eol
+def cells(cellWidth:Int = 3, cellNum: Int = 3) =
+  ("|" + " " * cellWidth) * cellNum + "|" + eol
+def mesh(cellWidth: Int = 3, cellNum: Int =  3) =
+  (bar(cellWidth, cellNum)
+    + cells(cellWidth, cellNum)) * 2
+    + bar(cellWidth, cellNum)
